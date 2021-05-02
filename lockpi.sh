34e492e12e02e4b5aac581693f71fd55425322cd
nya-furball <nyan_furball@protonmail.com>
2021-05-02 19:38:08 +0000

    beta release

--- lockpi.sh
+++ lockpi.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # modified shell to avoid bugs for now
 # return to sh once posix compliant
-# #!/bin/sh
+##!/bin/bash
 
 # Lockpi Installer:
 # Headlessly install linux distributions through a x86 computer onto
@@ -14,6 +14,17 @@
 # any damage incurred while using this script should you chose to use it.
 
 # TODO
+# Sane cryto setup
+#	Diceware random first time keyslot integration
+# FUCKING POSIX COMPLIANCE
+#	printf $VAR to printf %s $VAR or avoid entirely
+#	check for /run/shm
+# Support PC distros:
+#	Debian
+#		WHERE THE FUCK IS ADIANTUM?!
+#		CHROOT shenanigans
+#	Arch
+#		qemu-arm-static shenanigans
 # backup configs
 #	actual script
 #	chroot script
@@ -30,21 +41,24 @@
 #	NUKE rpi MS repo (only for raspios)
 #	include initramfs_config.txt for raspios, different initramfs for different kernels
 # nuke resize.sh on initial install
-# LUKS max iterations on pi hardware
+#	kali (systemd)
+#	debian (systemd?)
+
 # Arch comptiability
-#	fix no crypt_pi error after unlock
+#	fix install_arch() no crypt_pi error after unlock
 # command line args instead of prompt
 
 
 
 # TODO TESTS:
-# POSIX COMPLIANCE! (read -. and echo -.)
+# POSIX ???
+
 
 # HELPER FUNCTIONS
 checkroot(){
-	if [ "$EUID" -ne 0 ]
-		then echo "Please run as root";
-		exit;
+	if [ "$(id -u)" -ne 0 ]; then 
+		echo "Please run as root";
+		exit 1;
 	fi
 }
 
@@ -58,57 +72,102 @@ read_p(){
 
 
 # VARS
-TARGET_BLOCK_DEVICE="";
-USE_LUKS=1;
 IMAGE=???;
-echo "";
+#echo "";
 
 # check for prereqs
 prereq(){
-	#local prereqs=("lsblk" "sed" "mount" "umount" "mke2fs" "mkfs.fat" "cryptsetup" "bsdtar" );
+	#local prereqs=( "lsblk" "blkid" "sed" "mount" "umount" "mke2fs" "mkfs.fat" "cryptsetup" "qemu-arm-static" );
+	# note:
+	# leaving out bsdtar as arch is not supported rn
 	
 	# simple check for now bc we're pushing the code out fast
-	if [ ! -e /usr/bin/qemu-arm-static ]; then {
-		echo "qemu-arm-static not found. please install it!";
+	#if [ ! -e /usr/bin/qemu-arm-static ]; then {
+	#	echo "qemu-arm-static not found. please install it!";
+	#	exit 1;
+	#}
+	#fi;
+	
+	#for i in "${prereqs[@]}"; do {
+	#	echo "$i";
+	#} 
+	#done;
+	
+	# check if relevant programs exist
+	local prereqs="lsblk blkid sed mount umount mke2fs mkfs.fat cryptsetup qemu-arm-static rsync kmod diceware";
+	local prereqLength=$(echo "${prereqs}"|wc -w);
+	local currentItem="";
+	for i in $(seq 1 ${prereqLength}) ; do {
+		currentItem="$(echo ${prereqs}| cut -f $i -d' ')";
+		which "${currentItem}" 1>/dev/null || { printf "\nPrerequesites not met!\nYou don't have ${currentItem}.\nPlease check you have the following:\n${prereqs}\n\n"; exit 1; };
+	}
+	done;
+	
+	# check if kernel supports adiantum cipher
+	# Note:
+	#	WHY THE FUCK DOES DEBIAN NOT SUPPORT ADIANTUM?!
+	#cat /proc/crypto | egrep -Eq 'name[ \t]*:[ \t]*adiantum(xchacha12,aes)';
+	kmod list|cut -f1 -d' '|grep -iq adiantum;
+	if [ $? -eq 1 ]; then {
+		echo "Your kernel doesn't support the Adiantum Cipher right now.";
+		echo "Please either: "
+		echo "1: Load the kernel module 'adiantum' using modprobe";
+		echo "OR";
+		echo "2: Install a kernel that supports adiantum (likely at least version 5.x).";
 		exit 1;
 	}
 	fi;
-	
 }
 
 # guided prompts
+IMAGE_IMG="";
+TARGET_BLOCK_DEVICE="";
+LUKS_PASSPHRASE="";
 prompts(){
+	# reminder to unfreeze
+	echo "If you accidentally pressed ctrl+S, press ctrl+Q to unfreeze.";
+	echo "";
+	
 	# used for parsing, TODO
 	#read -e -p "Path to installation image: " IMAGE;
-	
-	read_p "Path to decompressed installation image (.img): " IMAGE_IMG;
+	while [ "${IMAGE_IMG}" = "" ]; do {
+		read_p "Path to decompressed installation image (.img): " IMAGE_IMG;
+	}
+	done;
 	
 	echo "LIST OF BLOCK DEVICES:";
 	lsblk -i -o 'NAME,LABEL,MODEL,SIZE,TYPE';
-	read_p "Enter full block device path to install (/dev/???): " TARGET_BLOCK_DEVICE;
-	
-	# passphrase entry and confirmation loop
-	LUKS_PASSPHRASE="";
-	local LUKS_PASSPHRASE_BUFFER="";
-	stty -echo;
-	while [ -z "${LUKS_PASSPHRASE}" -o "${LUKS_PASSPHRASE}" != "${LUKS_PASSPHRASE_BUFFER}" ]; do {
-		printf "Enter LUKS passphrase: " 
-		read LUKS_PASSPHRASE_BUFFER;
-		printf "\n";
-		
-		printf "Confirm LUKS passphrase: " 
-		read LUKS_PASSPHRASE;
-		printf "\n";
+	while [ "${TARGET_BLOCK_DEVICE}" = "" ]; do {
+		read_p "Enter full block device path to install (/dev/???): " TARGET_BLOCK_DEVICE;
 	}
 	done;
-	stty echo;
+	
+	# passphrase entry and confirmation loop
+	#LUKS_PASSPHRASE="";
+	#local LUKS_PASSPHRASE_BUFFER="";
+	#stty -echo;
+	#while [ -z "${LUKS_PASSPHRASE}" -o "${LUKS_PASSPHRASE}" != "${LUKS_PASSPHRASE_BUFFER}" ]; do {
+	#	printf "Enter LUKS passphrase: " 
+	#	read LUKS_PASSPHRASE_BUFFER;
+	#	printf "\n";
+	#	
+	#	printf "Confirm LUKS passphrase: " 
+	#	read LUKS_PASSPHRASE;
+	#	printf "\n";
+	#}
+	#done;
+	#stty echo;
+	
+	# Configure a firstboot LUKS keyslot with 10 word diceword passphrase (128 bit entropy)
+	LUKS_PASSPHRASE="$(diceware -w en_eff -d' ' -n 10 --no-caps)";
 	
 	# confirmation
 	printf "\nInstallation target: ${TARGET_BLOCK_DEVICE}\n";
+	echo "Installation image: ${IMAGE_IMG}";
 	echo "WARNING: ALL DATA WILL BE LOST ON TARGET.";
 	confirmation "Type UPPER CASE YES to proceed: " "YES";
 	if [ $? -eq "1" ]; then {
-		exit;
+		exit 1;
 	}
 	fi;
 }
@@ -118,8 +177,11 @@ prompts(){
 confirmation(){
 	local INPUT;
 	printf "$1";
-	read INPUT;
-	return $(printf %s "$INPUT" | grep -Fq "$2");
+	#read INPUT;
+	#printf %s "$INPUT" | grep -Fq "$2";
+	#return $?;
+	
+	read INPUT && printf "%s\\n" "$INPUT" | grep -Fiq $2;
 }
 
 # prototype:
@@ -132,6 +194,8 @@ detect_version(){
 }
 
 # partitioning
+USE_LUKS=1;
+LUKS_MAPPER="crypt_pi";
 format(){
 	#echo -e "o\np\nn\np\n1\n\n+200M\nt\nc\nn\np\n2\n\n\nw\n" | fdisk "${TARGET_BLOCK_DEVICE}";
 	printf "o\np\nn\np\n1\n\n+200M\nt\nc\nn\np\n2\n\n\nw\n\n" | fdisk "${TARGET_BLOCK_DEVICE}";
@@ -150,9 +214,20 @@ format(){
 	} ;
 	else {
 		#LUKS_MAPPER="$(echo ${ROOTPART}|sed -E -e 's/[/]dev[/]*//g' -e 's/(^.)/luks_\1/')";
-		LUKS_MAPPER="crypt_pi";
+
 		#cryptsetup -y -v luksFormat --type=luks2 --sector-size=4096 -c xchacha12,aes-adiantum-plain64 -s 256 -h sha512 "${ROOTPART}";
-		echo ${LUKS_PASSPHRASE} | cryptsetup luksFormat --force-password --type=luks2 --sector-size=4096 -c xchacha12,aes-adiantum-plain64 -s 256 -h sha512 "${ROOTPART}";
+		#echo ${LUKS_PASSPHRASE} | cryptsetup luksFormat --force-password --type=luks2 --sector-size=4096 -c xchacha12,aes-adiantum-plain64 -s 256 -h sha512 "${ROOTPART}";
+		#echo ${LUKS_PASSPHRASE} | cryptsetup luksFormat --force-password --type=luks2 --sector-size=4096 -c xchacha12,aes-adiantum-plain64 -s 256 -h sha512 --pbkdf argon2i --pbkdf-memory 100000 --pbkdf-parallel 1 --pbkdf-force-iterations 4 "${ROOTPART}";
+		
+		# we are formatting to the lowest denominator: pi0
+		# 128 bit entropy passphrase *should* mitigate issues.
+		# (in quotes bc cryptography is not the author's forte.)
+		# argon2i params:
+		#	timecost = 4; most common settings from benchmarks across x86 and pi
+		#	memory = 200MB; pi0/w only has 256MB and we have initrd
+		#	parallel = 1; pi0/w only has 1 core. more than 1 will FAIL to unlock.
+		echo ${LUKS_PASSPHRASE} | cryptsetup luksFormat --force-password --type=luks2 --sector-size=4096 -c xchacha12,aes-adiantum-plain64 -s 256 -h sha512 --pbkdf argon2i --pbkdf-memory 200000 --pbkdf-parallel 1 --pbkdf-force-iterations 4 "${ROOTPART}";
+		
 		#cryptsetup open "${ROOTPART}" "${LUKS_MAPPER}";
 		echo ${LUKS_PASSPHRASE} | cryptsetup open "${ROOTPART}" "${LUKS_MAPPER}";
 		mkfs.ext4 "/dev/mapper/${LUKS_MAPPER}";
@@ -280,16 +355,37 @@ cleanup(){
 	cryptsetup close "${LUKS_MAPPER}";
 	rmdir "${MOUNTDIR}/ROOT" "${MOUNTDIR}/BOOT";
 	rm "${CHROOT_SCRIPT_LOCATION}";
-	echo "Done";
+}
+
+cleanup_interrupt(){
+	printf "\nInterrupted. Cleaning up...\n";
+	echo "Installation NOT complete.";
+	if [ "${MOUNTDIR}" = "" ]; then {
+		cryptsetup close "${LUKS_MAPPER}";
+		exit 1;
+	}
+	fi;
+	
+	armchroot_cleanup;
+	cleanup;
+	exit 1;
+}
+
+configure_traps(){
+	# trap signals so user cannot accidentally exit
+	trap 'cleanup_interrupt' 1;	# SIGHUP
+	trap 'cleanup_interrupt' 2;	# SIGINT, ctrl + c
+	trap 'cleanup_interrupt' 3;	# SIGQUIT, ctrl + \
+	trap 'cleanup_interrupt' 15;	# SIGTERM
 }
 
 
 
 # distro specific scripts
-CHROOT_SCRIPT_RASPIOS='#!/bin/bash
+CHROOT_SCRIPT_RASPIOS='#!/bin/sh
 
 # return to sh once POSIX compliant
-# #!/bin/sh
+##!/bin/bash
 
 # TODO:
 # Auto setup initramfs images and different versions using initramfs_config.txt
@@ -301,7 +397,8 @@ sed -E -e "s/^#CRYPTSETUP=/CRYPTSETUP=y/" -i /etc/cryptsetup-initramfs/conf-hook
 
 # ---- Auto setup initramfs, not implemented yet
 # configure config.txt to use different initramfs images for each pi version
-#echo -e "# configuration for initrd, edit if using 64bit kernel.\ninclude initramfs_config.txt" |tee -a /boot/config.txt;
+#echo "# configuration for initrd, edit if using 64bit kernel." |tee -a /boot/config.txt;
+#echo "include initramfs_config.txt" |tee -a /boot/config.txt;
 
 # generate initramfs images
 #for VERSION in $(ls /lib/modules/); do
@@ -318,8 +415,12 @@ printf "Select kernel version for initrd: ";
 read VERSION;
 
 # generate and use initramfs
-#echo -e "\n# use initrd to unlock encrypted root\ninitramfs initramfs.gz followkernel" |tee -a /boot/config.txt;
-printf "\n# use initrd to unlock encrypted root\ninitramfs initramfs.gz followkernel\n" |tee -a /boot/config.txt;
+#echo "" |tee -a /boot/config.txt;
+#echo "# use initrd to unlock encrypted root" |tee -a /boot/config.txt;
+#echo "initramfs initramfs.gz followkernel" |tee -a /boot/config.txt;
+echo "" |tee -a /boot/config.txt;
+echo "# use initrd to unlock encrypted root" |tee -a /boot/config.txt;
+echo "initramfs initramfs.gz followkernel" |tee -a /boot/config.txt;
 mkinitramfs -o /boot/initramfs.gz "${VERSION}";
 
 # disable initial resize
@@ -361,11 +462,119 @@ armchroot_run_setup(){
 
 
 
+# Add hardware capability specific LUKS keyslot.
+LUKS_FIRSTBOOT_SCRIPT='#!/bin/sh
+# trap signals so user cannot accidentally exit
+stty -ctlecho
+trap "" 1;	# SIGHUP
+trap "" 2;	# SIGINT, ctrl + c
+trap "" 3;	# SIGQUIT, ctrl + \
+trap "" 15;	# SIGTERM
+trap "" 20;	# SIGTSTP, ctrl + Z
+
+# force user into setting up new LUKS keyslot on the hardware
+CRYPT_PARTITION=REPLACEMENT_CRYPTPART;
+
+
+# setup
+mount -a;
+echo "If you accidentally pressed ctrl+S, press ctrl+Q to unfreeze.";
+echo "";
+
+# get correct firstboot pw to add keyslot later
+PW_FIRSTBOOT="";
+isIncorrect=1;
+stty -echo;
+while [ "${isIncorrect}" != "0" ]; do {
+	printf "Enter firstboot password again: ";
+	read PW_FIRSTBOOT;
+	printf "\n";
+	echo "${PW_FIRSTBOOT}" | cryptsetup open --test-passphrase "${CRYPT_PARTITION}";
+	isIncorrect=$?;
+	
+	if [ "${isIncorrect}" != "0" ]; then {
+		echo "Firstboot password incorrect.";
+	}
+	fi;
+	
+	# debug
+	#echo ${isIncorrect};
+	#echo ${PW_FIRSTBOOT};
+}
+done;
+stty echo;
+
+
+# get new LUKS pw
+echo "";
+echo "Setting up your new LUKS password.";
+echo "Suggestion: use a diceware passphrase with at least 6 words with EFF dictionary.";
+PW_LUKS="";
+PW_LUKS_BUFFER="";
+stty -echo;
+while [ "${PW_LUKS}" != "${PW_LUKS_BUFFER}" -o "${PW_LUKS}" = "" ]; do {
+	printf "Enter new LUKS passphrase: ";
+	read PW_LUKS_BUFFER;
+	printf "\n";
+	
+	printf "Confirm new LUKS passphrase: ";
+	read PW_LUKS;
+	printf "\n";
+}
+done;
+stty echo;
+
+# debug
+#echo "${PW_LUKS_BUFFER}";
+#echo "${PW_LUKS}";
+
+
+# debug
+#echo ${PW_FIRSTBOOT} | cryptsetup luksFormat --force-password --type=luks2 --sector-size=4096 -c xchacha12,aes-adiantum-plain64 -s 256 -h sha512 --pbkdf argon2i --pbkdf-memory 200000 --pbkdf-parallel 1 --pbkdf-force-iterations 4 "${CRYPT_PARTITION}";
+
+
+# add new LUKS keyslot on encrypted partition using entered password and kill firstboot keyslot
+printf "${PW_FIRSTBOOT}\n${PW_LUKS}\n" | cryptsetup luksAddKey --force-password --type=luks2 -h sha512 --pbkdf argon2i "${CRYPT_PARTITION}";
+cryptsetup -q luksKillSlot "${CRYPT_PARTITION}" 0;
+
+# remove script startup from cmdline. oneshot functionality
+sed -E -e "s/[ \t]+init=\/usr\/sbin\/luks_firstboot.sh//g" -i /boot/cmdline.txt;
+
+echo "";
+echo "FIRST BOOT PASSWORD WILL BE INVALID AFTER REBOOT!";
+echo "USE THE NEW CONFIGURED PASSWORD TO UNLOCK YOUR PI!";
+echo "IF YOU LOSE YOUR NEW PASSWORD, THERE IS NO RECOVERY!";
+echo "Press enter to reboot.";
+read temp;
+
+# debug
+#stty echo;
+#exit 0;
+
+reboot -f;
+'
+
+install_newLUKS(){
+	echo "${LUKS_FIRSTBOOT_SCRIPT}" > "${MOUNTDIR}/ROOT/usr/sbin/luks_firstboot.sh";
+	chown root:root "${MOUNTDIR}/ROOT/usr/sbin/luks_firstboot.sh";
+	chmod 774 "${MOUNTDIR}/ROOT/usr/sbin/luks_firstboot.sh";
+	sed -E -e 's/(.$)/\1 init=\/usr\/sbin\/luks_firstboot.sh/' -i "${MOUNTDIR}/BOOT/cmdline.txt";
+	sed -E -e "s/REPLACEMENT_CRYPTPART/\/dev\/disk\/by-partuuid\/${UUID_ROOTPART}/" -i "${MOUNTDIR}/ROOT/usr/sbin/luks_firstboot.sh";
+}
+
+
+
+# disable accidental suspend from end user
+stty -ctlecho
+trap '' 20;	# SIGTSTP, ctrl + Z
 
 # main();
 checkroot;
 prereq;
 prompts;
+
+# configure cleanup procedure on interrupts
+configure_traps;
 
 # disk setup
 format;
@@ -383,6 +592,7 @@ install_img;
 install_configure_disks;
 # distro specific setup
 install_configure_setup;
+install_newLUKS;
 
 # debug: testing archinstall
 #echo "${CHROOT_SCRIPT_ARCH}" > "${CHROOT_SCRIPT_LOCATION}";
@@ -398,3 +608,10 @@ echo "Syncing disk. Please wait.";
 sync;
 armchroot_cleanup;
 cleanup;
+
+# Final prompts
+stty ctlecho;
+echo "";
+echo "Installation complete. Eject your SDcard and insert into pi.";
+echo "Your first time unlock password: ";
+echo "${LUKS_PASSPHRASE}";
